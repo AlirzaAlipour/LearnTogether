f9c6643836cc2af5b398544eb3c701a553cb8c5f
Alireza Alipour <al.alipour03@gmail.com>
2024-07-24 06:20:24 -0700

    add prifiles model

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,19 @@
 from django.db import models
+from django.conf import settings
+import uuid
 
-# Create your models here.
 
+class Subject (models.Model):
+    title = models.CharField(max_length=255)
+class TeacherProfile (models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='teacher_profile')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    bio = models.TextField(max_length=555)
+    subject = models.ForeignKey(Subject, on_delete=models.PROTECT)
+    phone_number = models.CharField(max_length=12)
+    
+
+class StudentProfile (models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='student_profile')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    phone_number = models.CharField(max_length=11)
